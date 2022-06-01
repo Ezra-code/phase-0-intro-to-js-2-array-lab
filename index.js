86ad5ba7aaa9988bbe6eb63f74e9e0b430d33efd
Ezra-code <njerukithinji872@gmail.com>
2022-06-01 08:41:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    let allCats = [...cats, name]
+    return allCats
+}
+// console.log(appendCat('otis'))
+function prependCat(name){
+    let firstCat = [name, ...cats]
+    return firstCat
+}
+function  removeFirstCat(){
+    let removeFCat = cats.slice(1, 3)
+    return removeFCat
+}
+function removeLastCat(){
+    let removeCat = cats.slice(0, 2)
+    return removeCat
+}
+console.log(removeLastCat())
